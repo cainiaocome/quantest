ac99a573ee6355d48b6aaacbb152082724426488
Yang Hui <yanghui@Yangs-Mac-mini.local>
2018-12-16 00:54:08 +0800

    delete files

--- SPSS modeler/download.py
+++
@@ -1,5 +0,0 @@
-import tushare as ts
-
-if __name__ == '__main__':
-    df = ts.get_hist_data('600810',start='2015-01-05',end='2017-03-13')
-    df.to_excel('/Users/yanghui/Documents/SPSS modeler/600810/600810.xlsx', encoding='GBK')
